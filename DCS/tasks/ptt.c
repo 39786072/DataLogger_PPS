b28a4bb956796cc424ca35cb697303fecfae3c2e
#Vincent <santiagocorrao96@outlook.com>
2020-05-13 00:02:43 -0300

    Add PTT_UpdateBuffers_Task, restartPeriodicValues, Ptt_TakeData_Task, initBuffers functions

--- DCS/tasks/ptt.c
+++ DCS/tasks/ptt.c
@@ -11,9 +11,10 @@
 #include "../task_support/DCS_buffer.h"
 #include "../system/system_1769_003-0_c03a.h"
 #include "../port/port_1769_003-0_c03a.h"
+#include "weather_station.h"
 
 typedef enum {
-	Init = 0, Test, WaitForACK, Wait,Send_Stage1,Send_Stage2
+	Init = 0, Test, WaitForACK, Wait, Send_Stage1, Send_Stage2
 } Ptt_State;
 typedef enum {
 	Ready = 0x8A, Busy = 0x85, ACK = 0xAA, NACK = 0x05
@@ -29,10 +30,38 @@ typedef enum {
 	LdInfo = 0x7D
 } Ptt_Commands;
 typedef enum {
-	Buf1,Buf2
+	Buf1, Buf2
 } Ptt_Buffers;
+
+/*==================[internal data declaration]==============================*/
+static uint8_t buffer[BUFFER_NUMBER][BUFFER_LENGTH]; /* Private data structure that represents buffers to be sent */
+static uint32_t samples; /* Private number of samples taken */
+static uint8_t bufferNumber; /* Private current buffer number */
+static uint32_t avgtemp, avgws; /* Private auxiliar variables to calculate averages */
+
+static void initBuffers() {
+	/* Internal variables */
+	uint8_t i, j;
+	/* For each buffer */
+	for (i = 0; i < BUFFER_NUMBER; i++) {
+		/* For each data value */
+		for (j = 0; j < BUFFER_LENGTH; j++) {
+			buffer[i][j] = 0;
+		}
+		/* Special Values */
+		buffer[i][LT] = 0x7f;
+		buffer[i][LT + 1] = 0xff;
+		buffer[i][LH] = 0x7f;
+	}
+
+	avgtemp = 0;
+	avgws = 0;
+	samples = 0;
+	bufferNumber = 0;
+}
 void PTT_Init() {
 	UART_Configure(LPC_UART0, 9600);
+	initBuffers();
 }
 
 void PTT_Start() {
@@ -70,7 +99,7 @@ void PTT_Init_StData(Ptt_Buffers buffer) {
 	//Envio del comando STDATA
 	BUFFER_Push(Ptt_Tx, StData | buffer);
 }
-void PTT_StData( uint8_t * data, uint8_t dataLength) {
+void PTT_StData(uint8_t * data, uint8_t dataLength) {
 	uint8_t i;
 	//Envio de la longitud en grupos de 4 bytes de acuerdo al manual de la ptt
 	BUFFER_Push(Ptt_Tx, (dataLength + 3) / 4);
@@ -122,28 +151,24 @@ void PTT_Task() {
 		if (StateCounter++ > WAIT_DATA_TIMES) {
 			SYSTEM_Perform_Safe_Shutdown();
 		}
-			aux = BUFFER_Pop(Ws_Rx);
-			if (aux != EMPTY_BUFFER_ERROR) {
-				if (aux == ACK) {
-					if (Ptt_Old_State == Init)
-					{
-						Ptt_Current_State = Wait;
-						Ptt_Old_State = WaitForACK;
-						StateCounter = 0;
-					}
-					else if (Ptt_Old_State == Send_Stage1)
-					{
-						Ptt_Current_State = Send_Stage2;
-						Ptt_Old_State = WaitForACK;
-						StateCounter = 0;
+		aux = BUFFER_Pop(Ws_Rx);
+		if (aux != EMPTY_BUFFER_ERROR) {
+			if (aux == ACK) {
+				if (Ptt_Old_State == Init) {
+					Ptt_Current_State = Wait;
+					Ptt_Old_State = WaitForACK;
+					StateCounter = 0;
+				} else if (Ptt_Old_State == Send_Stage1) {
+					Ptt_Current_State = Send_Stage2;
+					Ptt_Old_State = WaitForACK;
+					StateCounter = 0;
 
-					}
 				}
 			}
+		}
 		break;
 	case Wait:
-		if (StateCounter++ >= WAIT_TIMES)
-		{
+		if (StateCounter++ >= WAIT_TIMES) {
 			Ptt_Current_State = Send_Stage1;
 			Ptt_Old_State = Wait;
 		}
@@ -154,7 +179,7 @@ void PTT_Task() {
 		Ptt_Old_State = Send_Stage1;
 		break;
 	case Send_Stage2:
-		PTT_StData("s",1);//data y datalenght)
+		PTT_StData((uint8_t*) "s", 1); //data y datalenght)
 		Ptt_Current_State = Wait;
 		Ptt_Old_State = Send_Stage2;
 		break;
@@ -174,3 +199,146 @@ void PTT_TxRx(void) {
 		BUFFER_Push(Ptt_Rx, UART_ReceiveByte(LPC_UART0));
 	}
 }
+void Ptt_TakeData_Task(void) {
+	/* Internal variables */
+	uint16_t data;
+	/* 1 more sample */
+	samples++;
+	/* Update High Outside Temperature */
+	getCurrentOutsideTemperature((int16_t*) &data);
+	if (data
+			> (int16_t) ((buffer[bufferNumber][HT] << 8)
+					+ buffer[bufferNumber][HT + 1])) {
+		buffer[bufferNumber][HT] = (uint8_t) (data >> 8);
+		buffer[bufferNumber][HT + 1] = (uint8_t) data;
+	}
+	/* Update Low Outside Temperature */
+	if (data
+			< (int16_t) ((buffer[bufferNumber][LT] << 8)
+					+ buffer[bufferNumber][LT + 1])) {
+		buffer[bufferNumber][LT] = (uint8_t) (data >> 8);
+		buffer[bufferNumber][LT + 1] = (uint8_t) data;
+	}
+	/* Update Average Outside Temperature */
+	avgtemp += data;
+	/* Update High Outside Humidity */
+	getCurrentOutsideHumidity(&data);
+	if (data > buffer[bufferNumber][HH]) {
+		buffer[bufferNumber][HH] = (uint8_t) data;
+	}
+	/* Update Low Outside Humidity */
+	if (data < buffer[bufferNumber][LH]) {
+		buffer[bufferNumber][LH] = (uint8_t) data;
+	}
+	/* Update High Wind Speed */
+	getCurrentWindSpeed(&data);
+	if (data > buffer[bufferNumber][HWS]) {
+		buffer[bufferNumber][HWS] = (uint8_t) data;
+	}
+	/* Update Average Wind Speed */
+	avgws += data;
+}
+static void restartPeriodicValues() {
+	/* Restart High Outside Temperature */
+	buffer[bufferNumber][HT] = 0;
+	buffer[bufferNumber][HT + 1] = 0;
+
+	/* Restart Low Outside Temperature */
+	buffer[bufferNumber][LT] = 0x7f;
+	buffer[bufferNumber][LT + 1] = 0xff;
+	/* Restart Average Outside Temperature */
+	avgtemp = 0;
+	/* Restart High Outside Humidity */
+	buffer[bufferNumber][HH] = 0;
+
+	/* Restart Low Outside Humidity */
+	buffer[bufferNumber][LH] = 0x7f;
+
+	/* Restart High Wind Speed */
+	buffer[bufferNumber][HWS] = 0;
+
+	/* Restart Average Wind Speed */
+	avgws = 0;
+}
+
+void PTT_UpdateBuffers_Task(void) {
+	/* Internal variables */
+	uint16_t data;
+	uint8_t i, nob = 0;
+
+	/* Set NOB of all buffers */
+	for (i = 0; i < 3; i++) {
+		buffer[bufferNumber][NOB] = nob;
+		bufferNumber = (bufferNumber + 2) % 3;
+		nob++;
+	}
+	/* Update Outside Temperature */
+	getCurrentOutsideTemperature((int16_t*) &data);
+	buffer[bufferNumber][OT] = (uint8_t) (data >> 8);
+	buffer[bufferNumber][OT + 1] = (uint8_t) (data);
+	/* Update Average Outside Temperature */
+	avgtemp /= samples;
+	buffer[bufferNumber][AVGT] = (uint8_t) (avgtemp >> 8);
+	buffer[bufferNumber][AVGT + 1] = (uint8_t) (avgtemp);
+	/* Update Dew Point */
+	getCurrentDewPoint(&data);
+	buffer[bufferNumber][DP] = (uint8_t) (data >> 8);
+	buffer[bufferNumber][DP + 1] = (uint8_t) (data);
+	/* Update Outside Humidity */
+	getCurrentOutsideHumidity(&data);
+	buffer[bufferNumber][OH] = (uint8_t) (data);
+	/* Update Wind Speed */
+	getCurrentWindSpeed(&data);
+	buffer[bufferNumber][WS] = (uint8_t) (data);
+	/* Update Average Wind Speed */
+	buffer[bufferNumber][AVGWS] = (uint8_t) (avgws / samples);
+	/* Update Wind Chill */
+	getCurrentWindChill(&data);
+	buffer[bufferNumber][WC] = (uint8_t) (data >> 8);
+	buffer[bufferNumber][WC + 1] = (uint8_t) (data);
+	/* Update Wind Direction */
+	getCurrentWindDirection(&data);
+	buffer[bufferNumber][WD] = (uint8_t) (data >> 8);
+	buffer[bufferNumber][WD + 1] = (uint8_t) (data);
+	/* Update Daily Rain */
+	getCurrentDailyRain(&data);
+	buffer[bufferNumber][DR] = (uint8_t) (data >> 8);
+	buffer[bufferNumber][DR + 1] = (uint8_t) (data);
+	/* Update Yearly Rain */
+	getCurrentYearlyRain(&data);
+	buffer[bufferNumber][YR] = (uint8_t) (data >> 8);
+	buffer[bufferNumber][YR + 1] = (uint8_t) (data);
+	/* Update Barometric Pressure */
+	getCurrentBarometricPressure(&data);
+	buffer[bufferNumber][BP] = (uint8_t) (data >> 8);
+	buffer[bufferNumber][BP + 1] = (uint8_t) (data);
+	/* Update Battery Voltage */
+	getCurrentBatteryVoltage(&data);
+	buffer[bufferNumber][BV] = (uint8_t) (data >> 8);
+	buffer[bufferNumber][BV + 1] = (uint8_t) (data);
+	/* Set CRC */
+	buffer[bufferNumber][CRC] = (uint8_t) (buffer[bufferNumber][OT]
+			+ buffer[bufferNumber][OT + 1] + buffer[bufferNumber][HT]
+			+ buffer[bufferNumber][HT + 1] + buffer[bufferNumber][LT]
+			+ buffer[bufferNumber][LT + 1] + buffer[bufferNumber][AVGT]
+			+ buffer[bufferNumber][AVGT + 1] + buffer[bufferNumber][BP]
+			+ buffer[bufferNumber][BP + 1] + buffer[bufferNumber][OH]
+			+ buffer[bufferNumber][HH] + buffer[bufferNumber][LH]
+			+ buffer[bufferNumber][WS] + buffer[bufferNumber][HWS]
+			+ buffer[bufferNumber][AVGWS] + buffer[bufferNumber][WD]
+			+ buffer[bufferNumber][WD + 1] + buffer[bufferNumber][YR]
+			+ buffer[bufferNumber][YR + 1] + buffer[bufferNumber][DR]
+			+ buffer[bufferNumber][DR + 1] + buffer[bufferNumber][WC]
+			+ buffer[bufferNumber][WC + 1] + buffer[bufferNumber][DP]
+			+ buffer[bufferNumber][DP + 1] + buffer[bufferNumber][BV]
+			+ buffer[bufferNumber][BV + 1]);
+	/* Update buffer and it's variables*/
+	bufferNumber = (bufferNumber + 1) % BUFFER_NUMBER;
+	avgtemp = 0;
+	avgws = 0;
+	samples = 0;
+	/* Restart periodic values */
+	restartPeriodicValues();
+
+}
+
--- DCS/tasks/ptt.h
+++ DCS/tasks/ptt.h
@@ -8,6 +8,30 @@
 #ifndef TASKS_PTT_H_
 #define TASKS_PTT_H_
 
+/*==================[macros]=================================================*/
+#define BUFFER_NUMBER		3
+#define BUFFER_LENGTH		32
+
+#define CRC				0	/* Last byte of two's complement sum (1 byte)*/
+#define NOB				1	/* Number of Observation (1 byte) */
+#define IDUA			2	/* Interval from last sample (2 bytes) */
+#define OT 				4	/* Outside Temperature (2 bytes) */
+#define AVGT 			6	/* Average Outside Temperature (2 bytes) */
+#define HT	 			8	/* High Outside Temperature (2 bytes) */
+#define LT	 			10	/* Low Outside Temperature (2 bytes) */
+#define DP	 			12	/* Dew Point (2 bytes) */
+#define OH	 			14	/* Outside Humidity (1 byte) */
+#define HH	 			15	/* High Outside Humidity (1 byte) */
+#define LH	 			16	/* Low Outside Humidity (1 byte) */
+#define WS	 			17	/* Wind Speed (1 byte) */
+#define AVGWS	 		18	/* Average Wind Speed (1 byte) */
+#define HWS	 			19	/* High Wind Speed (1 byte) */
+#define WC	 			20	/* Wind Chill (2 bytes) */
+#define WD	 			22	/* Wind Direction (2 bytes) */
+#define BP	 			24	/* Barometric Pressure (2 bytes) */
+#define DR	 			26	/* Yearly Rain (2 bytes) */
+#define YR	 			28 	/* Daily Rain (2 bytes) */
+#define BV	 			30	/* Battery Voltage (2 bytes) */
 
 #define WAIT_TIMES						6000
 #define WAIT_DATA_TIMES					100
@@ -15,6 +39,7 @@
 void PTT_Init(void);
 void PTT_TxRx(void);
 void PTT_Task(void);
-
+void Ptt_TakeData_Task(void);
+void PTT_UpdateBuffers_Task(void);
 
 #endif /* TASKS_PTT_H_ */
